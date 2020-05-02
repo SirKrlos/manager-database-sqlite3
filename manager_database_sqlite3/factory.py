fd222b5ae2b0e7912a0fbcf7a7ae6a45fc6abdac
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-05-01 21:42:24 -0300

    Criando função factory 'keysvaluesToListStr'

--- manager_database_sqlite3/factory.py
+++ manager_database_sqlite3/factory.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+
+def keysvaluesToListStr (
+	keysvalues: dict = None
+) -> str :
+	"""Keysvalues to list str 'key value, key2 value2'"""
+
+	result = ""
+
+	for k,v in keysvalues.items ():
+		if result != "":
+			result += ", "
+		result += str ( f"`{k}`" ) + " " + str ( v )
+
+	return result
+
