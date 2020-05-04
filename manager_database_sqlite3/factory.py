73a4a569edfd10f37b3ffa03ad309c19ee5d61ad
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-05-04 19:26:01 -0300

    Criando função factory 'keysvaluesToDictStr'

--- manager_database_sqlite3/factory.py
+++ manager_database_sqlite3/factory.py
@@ -15,3 +15,18 @@ def keysvaluesToListStr (
 
 	return result
 
+def keysvaluesToDictStr (
+	keysvalues: dict = None,
+	sep: str = ","
+) -> str :
+	"""Keysvalues to dict str 'key = value, key2 = value2'"""
+
+	sets = ""
+	for k, v in keysvalues.items():
+		if sets != "": sets += f"{sep}"
+		sets += str(k) if type(k) == int else "`"+str(k)+"`"
+		sets += "="
+		sets += str(v) if type(v) == int else "'"+str(v)+"'"
+
+	return sets
+
